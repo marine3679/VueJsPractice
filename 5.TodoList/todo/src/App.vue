1b7326066e61efc72f2c39d28cfdbc09875d734b
gim.beomuk <gim.beomuk@flect.co.jp>
2019-04-03 18:44:21 +0900

    todolist

---
+++ 5.TodoList/todo/src/App.vue
@@ -0,0 +1,69 @@
+<template>
+  <div id="app">
+    <div class="container">
+      <div class="col-md-6 offset-md-3">
+        <h1 class="text-center mb-4">Todo Application</h1>
+        <input type="text" class="form-control mb-4" v-model="userInput" @keyup.enter="addNewTodo">
+        <div class="list-group md-4">
+          <template v-for="todo in activeTodoList">
+            <todo :label="todo.label" @componentClick="updateToDone(todo)"/>
+          </template>
+        </div>
+
+        <div class="text-right">
+          <button type="button" class="btn btn-sm" @click="changeCurrentState('active')">Todo</button>
+          <button type="button" class="btn btn-sm" @click="changeCurrentState('done')">Finish</button>
+          <button type="button" class="btn btn-sm" @click="changeCurrentState('all')">All</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import Todo from './components/Todo';
+export default {
+  name: 'app',
+  data() {
+    return {
+      userInput:'',
+      todoList: [],
+      currentState : 'active'
+    };
+  },
+  computed:{
+    activeTodoList() {
+      return this.todoList.filter(todo => this.currentState === 'all' || todo.state === this.currentState);
+    }
+  },
+  methods:{
+    changeCurrentState(state) {
+      this.currentState = state;
+    },
+    addNewTodo() {
+      this.todoList.push({
+        label:this.userInput,
+        state:'active'
+      });
+      this.userInput = '';
+    },
+    updateToDone(todo) {
+      todo.state = todo.state === 'active' ? 'done' : 'active';
+    }
+  },
+  components: {
+    Todo
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
